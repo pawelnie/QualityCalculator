a9c096db4517961bc2de158920fc18e871383166
Paweł Nieściur <niesciur@d17-l427-pc-01.ipa.iisg.agh.edu.pl>
2019-01-12 15:56:13 +0100

    Ostatni commit na zajeciach

--- app/src/androidTest/java/pl/edu/agh/qualitycalculator/MainActivityTest.java
+++ app/src/androidTest/java/pl/edu/agh/qualitycalculator/MainActivityTest.java
@@ -13,7 +13,6 @@ import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static org.junit.Assert.fail;
 
 @RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
@@ -29,5 +28,29 @@ public class MainActivityTest {
         onView(withId(R.id.btnAdd)).perform(click());
         onView(withId(R.id.tvResult)).check(matches(withText("4.0 plus 4.0 gives value 8.0")));
        }
+
+    @Test
+    public void testSubtract(){
+        onView(withId(R.id.etNum1)).perform(click()).perform(typeText("4"));
+        onView(withId(R.id.etNum2)).perform(click()).perform(typeText("4"));
+        onView(withId(R.id.btnSub)).perform(click());
+        onView(withId(R.id.tvResult)).check(matches(withText("4.0 minus 4.0 gives value 0.0")));
+    }
+
+    @Test
+    public void testMultiply(){
+        onView(withId(R.id.etNum1)).perform(click()).perform(typeText("4"));
+        onView(withId(R.id.etNum2)).perform(click()).perform(typeText("4"));
+        onView(withId(R.id.btnMult)).perform(click());
+        onView(withId(R.id.tvResult)).check(matches(withText("4.0 multiplied by 4.0 gives value 16.0")));
+    }
+
+    @Test
+    public void testDivide(){
+        onView(withId(R.id.etNum1)).perform(click()).perform(typeText("4"));
+        onView(withId(R.id.etNum2)).perform(click()).perform(typeText("4"));
+        onView(withId(R.id.btnDiv)).perform(click());
+        onView(withId(R.id.tvResult)).check(matches(withText("4.0 divided by 4.0 gives value 1.0")));
+    }
 }
 
